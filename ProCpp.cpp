7e15e45dc4e2e2e718d0f4b50f84408237ccb51f
Cynacyn <32937411+Cynacyn@users.noreply.github.com>
2022-09-20 14:19:52 +0300

    playing ranges

--- ProCpp.cpp
+++ ProCpp.cpp
@@ -3,11 +3,47 @@ import <format>;
 
 import <exception>;
 
+import <memory>;
 
-import Misc;
-import ExceptionsLab;
+import <vector>;
+import <iterator>;
+import <span>;
+import <ranges>;
+
+import <numbers>;
+
+//import Misc;
+//import ExceptionsLab;
+
+using std::begin;
+using std::end;
+
+using UnderlyingNumericType = int;
+UnderlyingNumericType constexpr from{ 1 };
+UnderlyingNumericType constexpr much{ 33 };
+UnderlyingNumericType constexpr insurance{ 8 };
+
+UnderlyingNumericType constexpr divisor{ 3377 };
+
+auto lambda_filter_by_divisor = [](UnderlyingNumericType i) 
+{ 
+    bool mod{ i % divisor };
+    
+    return mod;
+};
+
+auto lambda_transform = [](UnderlyingNumericType i)
+{
+    UnderlyingNumericType transformed 
+};
 
 int main()
 {
-    testDefaultParametersInOverridenMethods();
+    auto look{ std::views::iota(from) | std::views::filter([](UnderlyingNumericType) {return true; }) | std::views::take(much) };
+
+    std::vector< int > numbers;
+    numbers.reserve(much * insurance);
+    std::ranges::copy(look, std::back_inserter(numbers));
+    std::ranges::copy(numbers, std::ostream_iterator<int>(std::cout, " "));
+
 }
