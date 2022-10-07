ae2bcd7f74d3fabcf423b4e99a4361a3b7980b60
Cynacyn <tutnedaleko@gmail.com>
2022-10-07 08:58:15 +0300

    AddableC, AccumValue

--- ProCpp.cpp
+++ ProCpp.cpp
@@ -1,11 +1,15 @@
 import <iostream>;
+import <type_traits>;
+import <concepts>;
+
+import type_name;
 
 // Approach 1. Concept.
 template <typename Init, typename T > 
-concept AddableC = requires (T&& t) { Init{} + t; };
+concept AddableC = requires (T&& t) { Init{} + t; t + Init{}; };
 
-template <typename Init, typename T>
-Init AccumValue(T const&)
+template <typename Init, typename T >
+Init AccumValue(T const& value)
 {
     return Init{};
 }
@@ -52,8 +56,8 @@ int main()
     auto result1{ Accum<int>(10, 20, 1.1) }; // works
     std::cout << result1 << std::endl;
 
-    //auto result2{ Accum<int>(20, 40, 2.2, "string")}; // doesn't compile
-    //std::cout << result2 << std::endl;
+    auto result2{ Accum<int>(20, 40, 2.2, "string")}; // doesn't compile
+    std::cout << result2 << std::endl;
 
     auto result3{ Sum< double >(1, 2, 3.3, "asda") + Sum< double >()}; // works
     std::cout << result3 << std::endl;
